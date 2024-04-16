bc9306e5c4f2a6122c4e5ca7100946193a943caa
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-04-16 14:51:21 +0200

    adding products to home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import products from '../products'
 import {Row, Col} from 'react-bootstrap'
+import Product from '../components/Products'
 
 function HomeScreen() {
   return (
     <div>
         <h1>Latest Products</h1>
         <Row>
-            {products.map(products => (
-                <Col key={products._id} sm={12} md={6} lg={4} xl={3}>
-                    <h3>{products.name}</h3>
+            {products.map(product => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                    <Product product={product}/>
                 </Col>
             ))}
         </Row>
