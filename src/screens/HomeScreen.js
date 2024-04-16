fe814d998a466c829b09bf97a8bac8c11ccff710
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-04-16 13:17:15 +0200

    editing home scree component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import products from '../products'
+import {Row, Col} from 'react-bootstrap'
 
 function HomeScreen() {
   return (
-    <div>Latest Products</div>
+    <div>
+        <h1>Latest Products</h1>
+        <Row>
+            {products.map(products => (
+                <Col key={products._id} sm={12} md={6} lg={4} xl={3}>
+                    <h3>{products.name}</h3>
+                </Col>
+            ))}
+        </Row>
+    </div>
   )
 }
 
