9b2d641d24824f3f700e09967ee576cf6c399eb8
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-10 16:59:49 +0200

    adding const to RegisterScreen

---
+++ src/screens/RegisterScreens.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import FormContainer from '../components/FormContainer'
+import { login } from '../actions/userActions'
+
+function RegisterScreens() {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState('')
+
+
+  return (
+    <div>RegisterScreens</div>
+  )
+}
+
+export default RegisterScreens
