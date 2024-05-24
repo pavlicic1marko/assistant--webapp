e1bf001d31aaf2f1a49d0077e40c86d43177042b
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-25 00:26:58 +0200

    if user is loged in ,display user name on HOME page

--- src/screens/NewHomeScreen.js
+++ src/screens/NewHomeScreen.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useDispatch, useSelector} from 'react-redux'
 
 function NewHomeScreen() {
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
   return (
     <div>
-        <h2>Welcome to the AI App</h2>
+        <h2>Welcome {userInfo ? (<h5>{userInfo.name}</h5>):(<h5></h5>)} to the AI App</h2>
     </div>
   )
 }
