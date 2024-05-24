a5953f2d12f678df57dd86b9ff20fda429fd6e89
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-24 13:39:22 +0200

    remove rating

--- src/screens/TODOScreen.js
+++ src/screens/TODOScreen.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Button, Card} from 'react-bootstrap'
-import Rating from '../components/Rating'
 import { useParams } from 'react-router-dom';
 import axios from 'axios'
 
